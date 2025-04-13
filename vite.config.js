b1b04f98b03525834f40b1a228062896c539b25e
Hossam-97 <hossam_1997@live.com>
2025-04-13 03:25:03 -0400

    recommit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/Front_End_Using_React-",
+    base: "/Front-End-Using-React-",
   plugins: [react()],
 })
